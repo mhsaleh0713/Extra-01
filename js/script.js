3571c6840a55077667b5a648a73615b09e6b866e
mhsaleh0713 <95868137+mhsaleh0713@users.noreply.github.com>
2023-11-07 13:40:59 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-let playerOne = document.querySelector(".playerOne")
-let playerOneInput = document.querySelector(".playerOneInput")
-let playerOneBtn = document.querySelector(".playerOneBtn")
-let playerTwo = document.querySelector(".playerTwo")
-let playerTwoInput = document.querySelector(".playerTwoInput")
-let playerTwoBtn = document.querySelector(".playerTwoBtn")
-let para = document.querySelector(".para")
-let count = 3
-
+let playerone = document.getElementById("playerOne");
+let playerOneInput = document.getElementById("playerOneInput");
+let playerOneBtn = document.getElementById("playerOneBtn");
+let playerTwo = document.getElementById("playerTwo");
+let playerTwoInput = document.getElementById("playerTwoInput");
+let playerTwoBtn = document.getElementById("playerTwoBtn");
+let para = document.getElementById("para");
+let count = 3;
 
 playerOneBtn.addEventListener("click", () => {
-    if (playerOneInput.value > 0 && playerOneInput.value < 10){
-        playerOne.style="display: none "
-        playerTwo.style="display: block "
-    }
-    else {
-        para.innerHTML = "Please Type a number between 1 to 9"
-    }
-})
+  if (playerOneInput.value > 0 && playerOneInput.value < 10) {
+    playerOne.style.display = "none";
+    playerTwo.style.display = "block";
+  } else {
+    para.innerHTML = "Please type a number between 1 to 9.";
+  }
+});
+
 playerTwoBtn.addEventListener("click", () => {
-    if (playerTwoInput.value > 0 && playerTwoInput.value < 10){
-       if (count > 0){
-               if (playerOneInput.value == playerTwoInput.value){
-            para.style.color = "greenyeallow"
-            para.innerHTML = "Congrats"
-            count = 0
-           }else if (count > 0){
-            para.style.color = "orange"
-            para.innerHTML = "Try again! <br> Remainin Chances: " + count-- + "."
-           }else{
-            para.innerHTML = "Game over! <br> Please reload the page to play again."
-            para.style.color = "crimson"
-           }
+  if (playerTwoInput.value > 0 && playerTwoInput.value < 10) {
+    if (count > 0) {
+      if (playerOneInput.value == playerTwoInput.value) {
+        para.style.color = "greenyeallow";
+        para.innerHTML = "Congrats. <br> Reload the page to play again!";
+      } else {
+        para.style.color = "crimson";
+        para.innerHTML = "Try again. <br> Remaining Chances:  " + count-- + ".";
+      }
+    } else {
+      para.style.color = "crimson";
+      para.innerHTML = "Game over! <br> Reload the page to play again!";
+      count = 0;
     }
-    else {
-        para.innerHTML = "Please Type a number between 1 to 9."
-    }
-})
+  } else {
+    para.style.color = "crimson";
+    para.innerHTML = "Please type a number between 1 to 9.";
+  }
+});
